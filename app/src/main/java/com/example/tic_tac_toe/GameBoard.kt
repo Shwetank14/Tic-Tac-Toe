1c89d2d990c5e7742cee5c8171acdca120ce63f2
Shwetank14 <dshwetank1@gmail.com>
2021-10-08 23:43:56 +0530

    Added Files Via Commit (8th October)

---
+++ app/src/main/java/com/example/tic_tac_toe/GameBoard.kt
@@ -0,0 +1,313 @@
+package com.example.tic_tac_toe
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class GameBoard : AppCompatActivity(), View.OnClickListener {
+    // Current Player status
+    // Shows which player is currently is going for its move
+    // Initially playerX will start the game
+    var PLAYER_X = false
+    var PLAYER_Y = true
+
+    // Player Names
+    lateinit var playerX_Name: String
+    lateinit var playerY_Name: String
+
+    // Count of Records of each players win
+    var playerXWinCount = 0
+    var playerYWinCount = 0
+
+
+    // Total Number of moves performed on the board
+    var totalMoves = 0
+
+
+    // Logic Board
+    var boardStatus = Array(3){IntArray(3)}
+
+    // Display board
+    lateinit var board : Array<Array<Button>>
+
+
+
+    // @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_board)
+
+        // Getting ID's of all the buttons of the board -----------------------------------------------------------
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+        val button6 = findViewById<Button>(R.id.button6)
+        val button7 = findViewById<Button>(R.id.button7)
+        val button8 = findViewById<Button>(R.id.button8)
+        val button9 = findViewById<Button>(R.id.button9)
+        val resetButton = findViewById<Button>(R.id.reset)
+        val home = findViewById<Button>(R.id.home)
+        val resetScore = findViewById<Button>(R.id.resetScore)
+        // ----------------------------------------------------------------------------------------------------------
+
+        // Text View ID's
+        // Getting the names of the players from the home page
+        val player1 = intent.getStringExtra("Player1")
+        val player2 = intent.getStringExtra("Player2")
+
+
+        // Displaying the player names on to the screen
+        val player1TextView = findViewById<TextView>(R.id.tv1)
+        val player2TextView = findViewById<TextView>(R.id.tv2)
+
+        player1TextView.text = "$player1: "
+        player2TextView.text = "$player2: "
+
+        playerX_Name = player1.toString()
+        playerY_Name = player2.toString()
+
+
+        // Current Player Text
+        val currentPlayerText = findViewById<TextView>(R.id.mainTextView)
+        currentPlayerText.text = playerX_Name
+
+
+        // board Processing -----------------------------------------------------------------
+
+        board = arrayOf(
+            arrayOf(button1,button2,button3),
+            arrayOf(button4,button5,button6),
+            arrayOf(button7,button8,button9)
+        )
+        // Initializing the logic board
+        for(row in 0..2){
+            for(col in 0..2){
+                boardStatus[row][col] = -1
+            }
+        }
+
+        // Setting the Onclick Listener on grid buttons
+        for(i in board){
+            for(button in i){
+                button.setOnClickListener(this)
+            }
+        }
+
+        // Reset Board Button
+        // Reset the board without changing the score
+        resetButton.setOnClickListener {
+            totalMoves = 0
+            initializeBoard()
+        }
+
+        // Home button
+        // Take back to the home page of the game
+        home.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+
+        // Reset Score Button
+        // Resets the players score as wel as the board
+        resetScore.setOnClickListener {
+            playerXWinCount = 0
+            playerYWinCount = 0
+            totalMoves = 0
+            updateScores(playerX_Name)
+            updateScores(playerY_Name)
+            initializeBoard()
+        }
+
+    }
+
+    // InitializeBoard Method initializes the board
+    private fun initializeBoard() {
+        for(row in 0..2){
+            for(col in 0..2){
+                board[row][col].isEnabled = true
+                board[row][col].text = ""
+                boardStatus[row][col] = -1
+            }
+        }
+    }
+
+    // OnClick method will be called when
+    // the grid buttons are pressed (board keys)
+    override fun onClick(view: View) {
+
+        // Changing the state of player after each button press
+        // at each press on Player state becomes true and others state becomes false
+        // the player having the true state will perform its move
+        PLAYER_X = !PLAYER_X
+        PLAYER_Y = !PLAYER_Y
+
+        // Updating the current Player text
+        val boardText = if (!PLAYER_X){
+            playerX_Name
+        } else{
+            playerY_Name
+        }
+        val currentPlayerText = findViewById<TextView>(R.id.mainTextView)
+        currentPlayerText.text = boardText.toString()
+
+
+        // Getting the Button id to perform the task on
+        // On Each Grid button press the updateValue method would be called
+        // which will update the grid text as "X" or "0" according to the player
+        when(view.id){
+            R.id.button1 -> updateValue(0,0)
+            R.id.button2 -> updateValue(0,1)
+            R.id.button3 -> updateValue(0,2)
+            R.id.button4 -> updateValue(1,0)
+            R.id.button5 -> updateValue(1,1)
+            R.id.button6 -> updateValue(1,2)
+            R.id.button7 -> updateValue(2,0)
+            R.id.button8 -> updateValue(2,1)
+            R.id.button9 -> updateValue(2,2)
+        }
+
+        // Increment Total Moves count after each button press and check the
+        // Winning status that if any player has won or the game has drawn
+        totalMoves += 1
+        checkWinStatus()
+    }
+
+    // updateValueMethod will update the value at board[row][col] as "X" or "0"
+    private fun updateValue(row: Int, col: Int) {
+        val currentVal: Int
+        val boardText: String
+        if (PLAYER_X){
+            boardText = "X"
+            currentVal = 1
+        } else{
+            boardText = "0"
+            currentVal = 0
+        }
+        board[row][col].text = boardText.toString()
+        board[row][col].isEnabled = false
+
+        boardStatus[row][col] = currentVal
+    }
+
+
+    // CheckWinStatus Method
+    // Will check if any player as won
+    private fun checkWinStatus() {
+        // The Method will check in all possible direction for the win or draw
+        // For each outcome a Toast Message would be displayed
+
+        // Diagonal Check
+        if(boardStatus[0][0] == boardStatus[1][1] && boardStatus[0][0] == boardStatus[2][2]){
+            if(boardStatus[0][0] == 1){
+                disableButton()
+                popOutToast(boardStatus[0][0])
+            }
+            else if(boardStatus[0][0] == 0){
+                disableButton()
+                popOutToast(boardStatus[0][0])
+            }
+            return
+        }
+
+        // Anti Diagonal Check
+        else if(boardStatus[0][2] == boardStatus[1][1] && boardStatus[0][2] == boardStatus[2][0])  {
+            if(boardStatus[0][2] == 1){
+                disableButton()
+                popOutToast(boardStatus[0][2])
+            }
+            else if(boardStatus[0][2] == 0){
+                disableButton()
+                popOutToast(boardStatus[0][2])
+            }
+            return
+        }
+
+
+        // Row Check
+        for(i in 0..2){
+            if(boardStatus[i][0] == boardStatus[i][1]  && boardStatus[i][0] == boardStatus[i][2]){
+                if(boardStatus[i][0] == 1){
+                    disableButton()
+                    popOutToast(boardStatus[i][0])
+                }else if(boardStatus[i][0] == 0){
+                    disableButton()
+                    popOutToast(boardStatus[i][0])
+                }
+                return
+            }
+        }
+
+
+        // Column Check
+        for(i in 0..2){
+            if(boardStatus[0][i] == boardStatus[1][i]  && boardStatus[0][i] == boardStatus[2][i]){
+                if(boardStatus[0][i] == 1){
+                    disableButton()
+                    popOutToast(boardStatus[0][i])
+
+                }else if(boardStatus[0][i] == 0){
+                    disableButton()
+                    popOutToast(boardStatus[0][i])
+                }
+                return
+            }
+        }
+
+        // If all boxes filled and No outcome i.e totalMoves has become 9 and no winner is there
+        if(totalMoves >= 9){
+            Toast.makeText(this,"Game draw please reset the board",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    // The Method Displays a toast message for the win or draw case
+    // called inside the checkWinStatus method
+    private fun popOutToast(playerValue: Int) {
+        val currWinner: String
+        if(playerValue == 1) {
+            playerXWinCount += 1
+            currWinner = playerX_Name
+            updateScores(currWinner)
+        }
+        else{
+            playerYWinCount += 1
+            currWinner = playerY_Name
+            updateScores(currWinner)
+        }
+
+        Toast.makeText(this, "$currWinner wins the game", Toast.LENGTH_SHORT).show()
+
+    }
+
+    // Will Update the player score after each game
+    private fun updateScores(currWinner: String) {
+        val score1 = findViewById<TextView>(R.id.score1tv)
+        val score2 = findViewById<TextView>(R.id.score2tv)
+        if(currWinner == playerX_Name){
+            score1.text = playerXWinCount.toString()
+        }
+        else if(currWinner == playerY_Name){
+            score2.text = playerYWinCount.toString()
+        }
+    }
+
+    // If anyone has won the game this method will disable
+    // the remaining empty grid cells
+    private fun disableButton() {
+        for(i in board){
+            for(button in i){
+                button.isEnabled = false
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.tic_tac_toe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Getting the ID's of the start button and the edit text views
+        val startGameButton = findViewById<Button>(R.id.btn1)
+        val plyr1 = findViewById<EditText>(R.id.plyr1)
+        val plyr2 = findViewById<EditText>(R.id.plyr2)
+
+
+        startGameButton.setOnClickListener {
+
+            // If the text entered in any of the edit texts spaces is empty the game will not start
+            if(plyr1.text.toString().isEmpty() or plyr2.text.toString().isEmpty()){
+                Toast.makeText(this,"Player Name Can't be empty", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                // Taking the player1 and player2 name values entered by the user and
+                // passing it to the GameBoard activity as a string
+                val newIntent = Intent(this,GameBoard::class.java)
+                newIntent.putExtra("Player1",plyr1.text.toString())
+                newIntent.putExtra("Player2", plyr2.text.toString())
+                startActivity(newIntent)
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finishAffinity()
+    }
+
+}
